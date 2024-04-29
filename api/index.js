8ffcb3500de304b21c88a242f9ef106c77995599
Femik84 <femik84@gmail.com>
2024-04-29 13:58:03 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 //note: we are able to use 'import' instead of using 'require' by changing the type in package.json to 'module' insted of the default "common js"
+// to avoid making our mongodb user name and password visible to the public we will not be using our connect string here instead in process.env..
+// and we need to install env to be able to use dotenv package
 
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const app = express();
 
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
 app.listen(3000, () => {
     console.log('server is running on port 3000')
 });
