a3ee03e8bfba12f4b116d2bb582c2e456d879a29
Femik84 <femik84@gmail.com>
2024-05-06 14:10:53 +0100

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,25 +1,26 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 // import themeReducer from './theme/themeSlice';
-// import { persistReducer, persistStore } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// const rootReducer = combineReducers({
-//   user: userReducer,
-//   theme: themeReducer,
-// });
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   version: 1,
-// };
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-     reducer: {
-          user: userReducer,
-     },
-})
-// export const persistor = persistStore(store);
+     reducer: persistedReducer,
+     middleware: (getDefaultMiddleware) =>
+          getDefaultMiddleware({ serializableCheck: false }),
+      });
+export const persistor = persistStore(store);
+
+//the above persist store created along with the store is to allow the rexdux extention in chrome to save the data even when reload
