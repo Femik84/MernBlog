3cb5d66812c4cc2d6a63bbabfc083ff252886be7
Femik84 <femik84@gmail.com>
2024-05-06 13:23:23 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,69 +1,31 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+
+
 // import OAuth from '../components/OAuth';
 
 export default function SignIn() {
-  // const [formData, setFormData] = useState({});
-  // const { loading, error: errorMessage } = useSelector((state) => state.user);
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  // const handleChange = (e) => {
-  //   setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
-  // };
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (!formData.email || !formData.password) {
-  //     return dispatch(signInFailure('Please fill all the fields'));
-  //   }
-  //   try {
-  //     dispatch(signInStart());
-  //     const res = await fetch('/api/auth/signin', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     const data = await res.json();
-  //     if (data.success === false) {
-  //       dispatch(signInFailure(data.message));
-  //     }
-
-  //     if (res.ok) {
-  //       dispatch(signInSuccess(data));
-  //       navigate('/');
-  //     }
-  //   } catch (error) {
-  //     dispatch(signInFailure(error.message));
-  //   }
-  // };
-
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate(); 
-
-
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-  
-  // the trim is used to remove unnnecessary space from the data input
-
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -71,19 +33,20 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
 
 
+  
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
